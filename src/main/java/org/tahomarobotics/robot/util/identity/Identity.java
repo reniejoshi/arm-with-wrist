34485242f62e6d0799711124b7d5efaeb33eef5c
Nukkel <110308379+nukkeldev@users.noreply.github.com>
2025-01-27 10:04:19 -0800

    BB Mac Address (#79)

--- src/main/java/org/tahomarobotics/robot/util/identity/Identity.java
+++ src/main/java/org/tahomarobotics/robot/util/identity/Identity.java
@@ -2,7 +2,7 @@ package org.tahomarobotics.robot.util.identity;
 
 @SuppressWarnings("ALL")
 public enum Identity {
-    BETA("00:00:00:00:00:00");
+    BETA("00:80:2f:32:fd:29");
 
     private final String addr;
 
